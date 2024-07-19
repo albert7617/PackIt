4fb0bb9784d30e03a89b5dec9a0ec38382a622a2
Albert <albert860728@gmail.com>
2024-07-19 21:07:25 +0800

    Change window title.

--- pack.py
+++ pack.py
@@ -210,7 +210,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Pack it"))
         self.pushButton.setText(_translate("MainWindow", "Select Folder"))
         self.runButton.setText(_translate("MainWindow", "Run"))
 
